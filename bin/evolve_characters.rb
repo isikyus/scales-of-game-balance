f027ea0b62a6d7670aab6ad80840842582db60f9
Isikyus <edward@palarang.com>
2018-02-04 17:04:19 +1100

    Use parser to load data for evolve_characters
    And set load path so that we can use `require` in library code.
    TODO: should probably have a /lib subdirectory in our program's name.

--- bin/evolve_characters.rb
+++ bin/evolve_characters.rb
@@ -2,42 +2,25 @@
 
 USAGE_MESSAGE = "Usage: ruby #{$1}"
 
-require_relative '../lib/genome'
-require_relative '../lib/build_option'
-require_relative '../lib/constraint'
-require_relative '../lib/character_factory'
+app_dir = File.dirname(File.dirname(File.realdirpath(__FILE__)))
+$: << "#{app_dir}"
+require 'lib/genome'
+require 'lib/parser'
+require 'lib/character_factory'
 
 require 'yaml'
 
 # Load game system data tables.
-app_dir = File.dirname(File.dirname(File.realdirpath(__FILE__)))
-data_file_path = File.join(app_dir, 'ogl', 'pathfinder.yml')
-data = YAML.load_file(data_file_path)
+data_file = File.new(File.join(app_dir, 'ogl', 'pathfinder.yml'))
+parser = Parser.new(data_file)
 
-# TODO: temove or put behind option.
-p data_file_path
-p data
-
-build_options = data['build_options'].map do |build_option|
-  effects = if build_option['effects']
-    BuildOption::Effect.from_array(build_option['effects'])
-  else
-    nil
-  end
-  BuildOption.new(build_option['name'], effects, build_option['choices'])
-end
-
-constraints = data['resources'].map do |resource|
-  Constraint.new(resource: resource['name'].to_sym,
-                 maximum: resource['maximum'])
-end
-@character_factory = CharacterFactory.new(constraints)
+@character_factory = CharacterFactory.new(parser.constraints)
 
 # Generate some random characters
 POPULATION_SIZE = 20
 GENOME_LENGTH = 5
 @population = POPULATION_SIZE.times.map do
-  Genome.new_randomised(GENOME_LENGTH, build_options)
+  Genome.new_randomised(GENOME_LENGTH, parser.build_options)
 end
 
 # Handy methods. TODO: build a class for this.
