28b931ebe9b5b6d5deb5f434c906e3448da6f212
Isikyus <edward@palarang.com>
2018-04-08 11:42:53 +1000

    Load parameters from command-line options
    Rather than hard-coding them all

--- bin/evolve_characters.rb
+++ bin/evolve_characters.rb
@@ -1,7 +1,5 @@
 # Evolve new randomly-generated characters
 
-USAGE_MESSAGE = "Usage: ruby #{$1}"
-
 app_dir = File.dirname(File.dirname(File.realdirpath(__FILE__)))
 $: << "#{app_dir}"
 require 'lib/genome'
@@ -13,7 +11,48 @@ require 'lib/character/reproducer'
 require 'lib/genome/reproducer'
 require 'lib/scorers/maximum_stat_scorer'
 
+require 'optparse'
 require 'yaml'
+
+# Default options for applying the genetic algorithm.
+algorithm_parameters = {
+  population: 20,
+  starting_genome_length: 10,
+  generations: 10,
+  survival_rate: 0.1,
+  mutation_chance: 0.05
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: ruby #{$1} [options]"
+
+  opts.on('-p', '--population POPULATION', Integer,
+          'Number of organisms in each generation') do |population|
+    algorithm_parameters[:population] = population
+  end
+
+  opts.on('--genome-size SIZE', Integer,
+          'Starting number of build choices in each genome; may change by mutations') do |genome_length|
+    algorithm_parameters[:starting_genome_length] = genome_length
+  end
+
+  opts.on('-g', '--generations COUNT', Integer,
+          'Number of generations to run for') do |generations|
+    algorithm_parameters[:generations] = generations
+  end
+
+  opts.on('-s', '--survival_rate RATE', Float,
+          'Proportion of characters selected as parents of the next generation') do |survival|
+    algorithm_parameters[:survival_rate] = survival
+  end
+
+  opts.on('-m', '--mutation_chance CHANCE', Float,
+          'Chance of a single mutation on reproducing. '\
+          'Applies recursively, so if it\'s 0.1, 1 in 10 organisms will mutate '\
+          'at least once, 1 in 100 at least twice, and so on') do |mutation_chance|
+    algorithm_parameters[:mutation_chance] = mutation_chance
+  end
+end.parse!
 
 # Load game system data tables.
 data_file = File.new(File.join(app_dir, 'ogl', 'sample.yml'))
@@ -24,11 +63,10 @@ parser = Parser.new(data_file)
 @name_generator = NameGenerator.new
 
 # Generate some random characters
-POPULATION_SIZE = 20
-GENOME_LENGTH = 10
-@population = POPULATION_SIZE.times.map do
-  genome = Genome.new_randomised(GENOME_LENGTH, parser.build_options)
-    name = @name_generator.call
+@population = algorithm_parameters[:population].times.map do
+  genome = Genome.new_randomised(algorithm_parameters[:starting_genome_length],
+                                 parser.build_options)
+  name = @name_generator.call
   @character_factory.build_character(genome, name)
 end
 
@@ -47,21 +85,20 @@ puts "Initial population:"
 print_population
 
 # Have highest-scoring individuals reproduce
-ITERATIONS = 10
-SURVIVAL_RATE = 0.1
-MUTATION_CHANCE = 0.05
-
 genome_reproducer = Genome::Reproducer.new(parser.build_options,
-                                           MUTATION_CHANCE,
+                                           algorithm_parameters[:mutation_chance],
                                            @random)
 reproducer = Character::Reproducer.new(@name_generator,
                                        genome_reproducer,
                                        @character_factory)
-@generation_runner = RunGeneration.new(@scorer, reproducer, SURVIVAL_RATE, @random)
+@generation_runner = RunGeneration.new(@scorer,
+                                       reproducer,
+                                       algorithm_parameters[:survival_rate],
+                                       @random)
 
-ITERATIONS.times do |iteration|
+algorithm_parameters[:generations].times do |generation|
   puts
-  puts "Iteration #{iteration + 1}:"
+  puts "Iteration #{generation + 1}:"
 
   @population = @generation_runner.call(@population)
 
